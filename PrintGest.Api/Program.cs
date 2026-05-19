3a339eacbfc83d194cf2ef18e7410034c84387ca
Kaio Vinicius <tidesenvolvimento9@amvox.com.br>
2026-05-19 12:23:26 -0300

    Update Program.cs

--- PrintGest.Api/Program.cs
+++ PrintGest.Api/Program.cs
@@ -20,8 +20,12 @@ builder.Services.AddCors(options =>
             .WithOrigins(
                 "http://localhost:5173",
                 "https://localhost:5173",
+                "http://localhost:5174",
+                "https://localhost:5174",
                 "http://127.0.0.1:5173",
-                "https://127.0.0.1:5173")
+                "https://127.0.0.1:5173",
+                "http://127.0.0.1:5174",
+                "https://127.0.0.1:5174")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
